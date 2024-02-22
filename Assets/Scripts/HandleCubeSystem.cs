8becb9770ce5e42530e07b53801d672a04ecff38
Bart <kmiecikjob@gmail.com>
2024-02-22 21:58:43 +0100

    Movement Authoring and playing with cube system

---
+++ Assets/Scripts/HandleCubeSystem.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public partial struct HandleCubeSystem : ISystem
+{
+    void OnUpdate(ref SystemState state)
+    {
+        foreach ((RefRW<LocalTransform> localTransform, RefRO<RotateSpeed> rotateSpeed, RefRO<Movement> movement) in 
+            SystemAPI.Query<RefRW<LocalTransform>, RefRO<RotateSpeed>, RefRO<Movement>>().WithNone<Player>())
+        {
+            localTransform.ValueRW = localTransform.ValueRO.RotateY(rotateSpeed.ValueRO.value * SystemAPI.Time.DeltaTime);
+            localTransform.ValueRW = localTransform.ValueRO.Translate(movement.ValueRO.value * SystemAPI.Time.DeltaTime);
+        }
+    }
+
+}
---
+++ Assets/Scripts/MovementAuthoring.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class MovementAuthoring : MonoBehaviour
+{
+    public class Baker : Baker<MovementAuthoring>
+    {
+        public override void Bake(MovementAuthoring authoring)
+        {
+            Entity entity = GetEntity(TransformUsageFlags.Dynamic);
+            AddComponent(entity, new Movement
+            {
+                value = new float3 (UnityEngine.Random.Range(-2,2), 0.5f, UnityEngine.Random.Range(-2, 2))
+            });
+        }
+    }
+}
+
+public struct Movement : IComponentData
+{
+    public float3 value;
+}
