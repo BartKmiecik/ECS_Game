b60ff2d6de73234b0c8f9026dcba85d55ce1408c
Bart <kmiecikjob@gmail.com>
2024-02-27 20:38:33 +0100

    House keeping structureAdding Enemy Tag authoring

---
+++ Assets/Scripts/Authoring/EnemyTagAuthoring.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class EnemyTagAuthoring : MonoBehaviour
+{
+    public class Baker : Baker<EnemyTagAuthoring>
+    {
+        public override void Bake(EnemyTagAuthoring authoring)
+        {
+            Entity entity = GetEntity(TransformUsageFlags.None);
+            AddComponent(entity, new EnemyTag { });
+        }
+    }
+}
+
+public struct EnemyTag : IComponentData
+{
+
+}
