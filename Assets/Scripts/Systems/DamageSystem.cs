231a235d8094d81fd6d3045f0d2239e88cdb0710
Bart <kmiecikjob@gmail.com>
2024-03-01 21:38:42 +0100

    Working destruction entity system

--- Assets/Scripts/Systems/DamageSystem.cs
+++ Assets/Scripts/Systems/DamageSystem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Entities.UniversalDelegates;
 using Unity.Transforms;
@@ -10,17 +11,22 @@ using UnityEngine;
 public partial struct DamageSystem : ISystem
 {
     int toHit;
+    EntityCommandBuffer ecb;
+    EntityManager em;
     public void OnCreate(ref SystemState state)
     {
         toHit = 5;
+        em = World.DefaultGameObjectInjectionWorld.EntityManager;
     }
 
     public void OnUpdate(ref SystemState state)
     {
         var space = Input.GetKeyDown("space");
+        ecb = new EntityCommandBuffer(Allocator.TempJob);
+        
         if (space){
             int i = 0;
-            foreach (RefRW<Health> health in SystemAPI.Query<RefRW<Health>>())
+            foreach ((RefRW<Health> health, Entity entity) in SystemAPI.Query<RefRW<Health>>().WithEntityAccess())
             {
                 while (health.ValueRW.isAlive && i < toHit)
                 {
@@ -28,12 +34,15 @@ public partial struct DamageSystem : ISystem
                     health.ValueRW.health -= 5;
                     if (health.ValueRW.health <= 0)
                     {
+                        
                         health.ValueRW.isAlive = false;
-                        //TODO destroy entity
+                        ecb.DestroyEntity(entity);
                     }
                 }
             }
         }
+        ecb.Playback(em);
+        ecb.Dispose();
     }
 
 /*    public partial struct IDamageJob : IJobEntity
